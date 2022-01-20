424a4f810195c868fc341fa824cd43332454c437
Hi-Im-Simon <szymol1001@gmail.com>
2022-01-20 13:40:30 +0100

    added a classmaker file

---
+++ classmaker.py
@@ -0,0 +1,29 @@
+# provide one argument on execution (name of the new class)
+import sys
+
+
+name = sys.argv[1]
+
+file = open(f'{ name }.hpp', 'w+')
+file.write(
+f"""#ifndef { f'{ name.upper() }_HPP' }
+#define { f'{ name.upper() }_HPP' }
+
+class { name } {{
+    public:
+
+    protected:
+
+}};
+
+#endif"""
+)
+file.close()
+
+file = open(f'{ name }.cpp', 'w+')
+file.write(
+f"""#include "{ name }.hpp"
+
+"""
+)
+file.close()
