3be4363777fc07341a37f6a55338d5b14ee92f48
Stanislaw <Billewski@gmail.com>
2022-02-15 09:58:25 +0100

    Minor corrections++

--- classes/Ship.hpp
+++ classes/Ship.hpp
@@ -5,16 +5,6 @@ class Ship : public Vehicle {
     private:
         int dock_depart_timer = 0;
         
-        std::string state;
-        // WHAT DIFFERENT STATES MEAN:
-        // idle - ship has 0/1 port in its route, so it will never depart
-        // departure - ship will start sailing in 15 minutes (with full speed to make it easier)
-        // sailing - ship alredy set to sea with full speed
-        // docking - ship will dock at a port in 15 minutes
-        // stopover - ship is in one of its connecting ports and waits (1 hour)
-        // arrived - ship has finished the whole cruise
-        // waiting for a spot - port is full and the ship is waiting for it to empty
-        
         std::vector<Harbor> route;
         std::string last_harbor;
 
